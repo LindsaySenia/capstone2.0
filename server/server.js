4833202b0ef0c69ca4a09c6a94d4c363265c468d
LindsaySenia <lmzsenia@gmail.com>
2021-11-29 11:07:31 -0500

    server adjustments

--- server/server.js
+++ server/server.js
@@ -3,24 +3,34 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const path = require('path')
+const path = require('path')
 const ctrl = require('./controllers')
 
-// const port = process.env.PORT || 3141;
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("public"));
+
+app.get("/styles", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/index.css"));
+});
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/index.js"));
+});
+
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname, '../index.html'));
 // });
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile('index.html');
+// });
 
-const port = process.env.PORT || process.env.SERVER_PORT;
+const port = process.env.PORT || 3141;
+// const port = process.env.PORT || process.env.SERVER_PORT;
 
 app.get('/login', ctrl.login);
 // app.post('/login/id', ctrl.register)
